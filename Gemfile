6452aa8da7313cebc0e94b2cfcce32e8ad177596
Glen Barnes <barnaclebarnes@masc.com>
2015-12-08 09:50:51 +1300

    Added .active class on menu items for current menu item. For #23

--- Gemfile
+++ Gemfile
@@ -10,8 +10,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'rails_12factor'
 
 
-
 group :development do
   gem 'spring'
+	gem 'byebug'
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def current_link_to label, path = nil, controller = nil
+		if controller
+	  	link_to label, path, class: (current_page?(controller: controller, action: action_name) ? "active" : nil)
+	  else
+	  	link_to label, path, class: (current_page?(path) ? "active" : nil)
+	  end	  	
+	end
 end
