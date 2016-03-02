fd96afdefc99c69ee2947de30390c8738f62f9fa
Adrian McEwen <adrianm@mcqn.com>
2016-03-02 19:22:40 +0000

    Add vanilla active_admin install.  Part of #35

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'pg', '~> 0.15'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
+gem 'devise'
+gem 'activeadmin', '1.0.0.pre2'
 
 gem 'rails_12factor'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :institutions, only: [:show, :index]
   resources :objects, only: [:show, :index], controller: 'things'
   resources :prints, only: [:index, :show]
