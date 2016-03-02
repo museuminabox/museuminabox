2bb83710a4018a48a2fa17efef2d01e97189cc87
Adrian McEwen <adrianm@mcqn.com>
2016-03-02 17:37:29 +0000

    Add basic User details. First part of #36

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :institutions, only: [:show, :index]
   resources :objects, only: [:show, :index], controller: 'things'
   resources :prints, only: [:index, :show]
